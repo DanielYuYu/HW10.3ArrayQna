4bc3b7a99bc56388dec5cb9152db36e68f99ba54
Daniel <daniel@macbook-pro.local>
2018-07-31 16:55:09 +0800

    Initial Commit

---
+++ HW10.3ArrayQna/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW10.3ArrayQna
+//
+//  Created by Daniel on 2018/7/31.
+//  Copyright © 2018年 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
